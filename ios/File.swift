a4be945c890aba89f523af171c3b6664ab04dbcf
Jay <jaypithava@xhtmljunkies.com>
2022-11-22 10:38:11 +0530

    first commit

---
+++ ios/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  SplashScreen
+//
+//  Created by nteam on 22/11/22.
+//
+
+import Foundation
---
+++ screen/SplashScreen.js
@@ -0,0 +1,57 @@
+import React, {useRef, useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import LottieView from 'lottie-react-native';
+import {useEffect} from 'react';
+
+const SplashScreen = props => {
+  const [authLoaded, setAuthLoaded] = useState(false);
+  const [animationLoaded, setAnimationLoaded] = useState(false);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setAuthLoaded(true);
+    }, 3000);
+  }, []);
+
+  const onAnimationFinish = () => {
+    setAnimationLoaded(true);
+  };
+
+  useEffect(() => {
+    if (authLoaded && animationLoaded) {
+      props.navigation.replace('Home');
+    }
+  }, [authLoaded, animationLoaded, props.navigation]);
+
+  return (
+    <View style={styles.root}>
+      <LottieView
+        ref={animation => {
+          ref.current = animation;
+        }}
+        style={styles.lottieView}
+        source={require('../assets/splash.json')}
+        autoPlay
+        loop={false}
+        onAnimationFinish={onAnimationFinish}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lottieView: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+});
+
+export default SplashScreen;
